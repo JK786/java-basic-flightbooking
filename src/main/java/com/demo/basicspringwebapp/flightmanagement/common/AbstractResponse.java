851bc1135b6355a205a86609393b7134da24b835
Jibran Khan <jibran@barraiser.com>
2024-03-18 01:36:06 +0530

    change

--- src/main/java/com/demo/basicspringwebapp/flightmanagement/common/AbstractResponse.java
+++ src/main/java/com/demo/basicspringwebapp/flightmanagement/common/AbstractResponse.java
@@ -1,7 +1,13 @@
 package com.demo.basicspringwebapp.flightmanagement.common;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
 @AllArgsConstructor
 public abstract class AbstractResponse {
 
--- src/main/java/com/demo/basicspringwebapp/flightmanagement/user/DTO/AddUserResponseDTO.java
+++ src/main/java/com/demo/basicspringwebapp/flightmanagement/user/DTO/AddUserResponseDTO.java
@@ -1,19 +1,22 @@
 package com.demo.basicspringwebapp.flightmanagement.user.DTO;
 
 import com.demo.basicspringwebapp.flightmanagement.common.AbstractResponse;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 
 @Getter
-@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
 public class AddUserResponseDTO extends AbstractResponse {
 
     private AddUserResponseDTO.User user;
 
     @Getter
-    @Builder(toBuilder = true)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @SuperBuilder(toBuilder = true)
     public static class User {
         private String id;
         private String name;
